8a09441eab7abf3a824eb98a55859ca32cbd41c0
Julien Esposito <julien.esposito@gmail.com>
2016-11-04 12:07:01 -0700

    Just a Reference on the Bridge Pattern for you to refer to
    Just a Reference on the Bridge Pattern for you to refer to

---
+++ Design_Patterns/BridgePattern_Revisited/BridgePattern_Revisited/IAbstractBridge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern_Revisited
+{
+    public interface IAbstractBridge
+    {
+        void Operation1();
+        void Operation2();
+    }
+}
